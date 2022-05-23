b4b7b06f6e0c4539ab47c46f252c78c38b0f7453
ryan-miyazato <ryan.miyazato@sptech.school>
2022-05-22 23:21:30 -0300

    Criado Home

--- public/js/main.js
+++ public/js/main.js
@@ -6,22 +6,25 @@ class myHeader extends HTMLElement {
             <img src="assets/logo3.png" alt="Logo Animoview" class="logo">
             <ul class="navBar">
                 <li>
-                <a href="" id="home_nav">Home</a>
+                <a href="index.html" class="navItem" id="home_nav">Home</a>
                 </li>
                 <li>
-                    <a href="" id="animes_nav">Animes</a>
+                    <a href="animes.html" class="navItem" id="animes_nav">Animes</a>
                 </li>
                 <li>
-                    <a href="" id="forum_nav">Fórum</a>
+                    <a href="pesquisa.html" class="navItem" id="pesquisa_nav">Pesquisa</a>
                 </li>
                 <li>
-                    <a href="" id="minhalista_nav">Minha Lista</a>
+                    <a href="forum.html" class="navItem" id="forum_nav">Fórum</a>
+                </li>
+                <li>
+                    <a href="minhalista.html" class="navItem" id="minhalista_nav">Minha Lista</a>
                 </li>
             </ul>
             <div class="registro">
-                <a class="botao_cadastro" href="">Cadastro</a>
+                <a class="botao-cadastro" href="">Cadastro</a>
                 <div class="linha"></div>
-                <a class="botao_login" href="">Login</a>
+                <a class="botao-login" href="">Login</a>
             </div>
         </div>
         </div>`
