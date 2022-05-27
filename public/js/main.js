380ad544095e9623e1814a80a421afa11aa43dde
raijonas <rai.maciel@bandtec.com.br>
2022-05-27 11:33:19 -0300

    Adicionado Footer

--- public/js/main.js
+++ public/js/main.js
@@ -32,3 +32,27 @@ class myHeader extends HTMLElement {
 }
 
 customElements.define('my-header', myHeader);
+
+class myFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="footer">
+            <div class="container">
+                <ul>
+                    <a href="">Home</a>
+                    <a href="">Animes</a>
+                    <a href="">Suporte</a>
+                    <a href="">Termos de Serviço</a>
+                    <a href="">Privacidade</a>
+                    <a href="">Cookies</a>
+                </ul>
+                <span>
+                    <div>Ryan Yuji Miyazato (RA 0221041) 1CCO, 2022</div>
+                    <div>Animoview © 2022</div>
+                </span>
+            </div>
+        </div>`
+    }
+}
+
+customElements.define('my-footer', myFooter);
